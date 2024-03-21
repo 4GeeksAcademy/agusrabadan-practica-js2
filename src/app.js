0ed1fd21dd47079144d9200d1cc96a632e605ee7
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-03-21 12:08:20 +0000

    queryselector

--- src/app.js
+++ src/app.js
@@ -38,8 +38,22 @@ window.onload = function() {
 
   let family = [person1, person2, person3, person4];
 
-  //Loop Map
+  /*Loop Map
   family.map(item => {
-    console.log(item.gender);
+    console.log(item.name, item.age);
+
+    item.hobbie.map(element => {
+      console.log("-" + element);
+    });
+  });*/
+  const tag = document.querySelector("#list");
+
+  let text = "";
+  family.map(item => {
+    text += "<h2>" + item.name + "</h2>";
+    item.hobbie.map(element => {
+      text += "<p>" + element + "</p>";
+    });
   });
+  tag.innerHTML = text;
 }; /* Fin */
